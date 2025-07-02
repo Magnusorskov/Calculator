72c3cf0e6cef23b52ee2cd37819f366036b475a3
Magnus Orskov <magnusorskov@hotmail.com>
2025-07-02 11:52:04 +0200

    add calculator functions

---
+++ Calculator.js
@@ -0,0 +1,12 @@
+function add(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+};
+
+function subtract(numberOne, numberTwo) {
+    return numberOne - numberTwo;
+};
+
+
+function multiply(numberOne, numberTwo) {
+    return numberOne - numberTwo;
+};
